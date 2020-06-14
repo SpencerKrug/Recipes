6d3886bcda8a1738bc8881975fbb5ef07f4b1c8a
Spencer Krug <spncrkrug@gmail.com>
2020-06-14 17:13:52 -0400

    Fixed database issues.

--- Recipes/Data/Contexts/RecipesDbContext.cs
+++ Recipes/Data/Contexts/RecipesDbContext.cs
@@ -2,6 +2,7 @@
 using Recipes.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,11 @@ namespace Recipes.Data.Contexts
     public class RecipesDbContext : DbContext
     {
         public RecipesDbContext(DbContextOptions<RecipesDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<RecipesModel>().ToTable("tbu_Recipes");
+        }
 
         public DbSet<RecipesModel> RecipesContext { get; set; }
     }
--- Recipes/Data/Models/RecipesModel.cs
+++ Recipes/Data/Models/RecipesModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Recipes.Data.Models
 {
     public class RecipesModel
     {
+        [Key]
         public Guid RecipeId { get; set; }
         public string RecipeName { get; set; }
         public string RecipeDescription { get; set; }
